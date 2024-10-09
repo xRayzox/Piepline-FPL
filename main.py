c7619d91400c5c22ad4c546be3d91921fff69560
Wael Hcine <wael.hcine@esprit.tn>
2024-10-09 22:26:43 +0100

    script updated

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import sys
+import os
 import pandas as pd
-sys.path.append('./FPL')
 
+sys.path.append(os.path.join(os.path.dirname(__file__), 'FPL'))
 from Functions import load_fixture, load_fpl, load_player_history
 from Transformation import (transform_fact_history, transform_gameweeks_data, 
                               transform_previous_season, transform_players, 
@@ -17,35 +18,44 @@ def load_fpl_data():
     transformed_positions_df = transform_positions(positions_of_players_df)
     transformed_gameweeks_df = transform_gameweeks_data(gameweeks_df)
 
-    # You can return the transformed data if needed
     return transformed_players_df, transformed_teams_df, transformed_positions_df, transformed_gameweeks_df
 
 # Define the function to load player statistics
 def load_player_statistics():
-    # Load players data
     players_df, _, _, _ = load_fpl()
     current_season_df, previous_seasons_df = load_player_history(players_df)
     
-    # Transform player history data
     transformed_current_season_df = transform_fact_history(current_season_df)
     transformed_previous_season_df = transform_previous_season(previous_seasons_df)
 
-    # You can return the transformed data if needed
     return transformed_current_season_df, transformed_previous_season_df
 
 def load_fixture_data():
     fixture_df = load_fixture()
     print(fixture_df)
 
+# Function to save DataFrame to CSV in the data folder
+def save_to_csv(df, file_name):
+    # Ensure the 'data' directory exists
+    os.makedirs('data', exist_ok=True)
+    
+    # Save the CSV in the 'data' folder
+    file_path = os.path.join('data', file_name)
+    df.to_csv(file_path, index=False)
+    print(f"Data saved to CSV file: {file_path}")
+
 # Main function to execute tasks in sequence
 def main():
     transformed_players, transformed_teams, transformed_positions, transformed_gameweeks = load_fpl_data()
     transformed_current_season, transformed_previous_season = load_player_statistics()
-    load_fixture_data()
 
-    # You can print or process the transformed data here
-    #print(transformed_players)
-    print(transformed_teams)
+    # Save the transformed DataFrames as CSV in the 'data' folder
+    save_to_csv(transformed_players, 'Players.csv')
+    save_to_csv(transformed_teams, 'Teams.csv')
+    save_to_csv(transformed_positions, 'Positions.csv')
+    save_to_csv(transformed_gameweeks, 'Gameweeks.csv')
+    save_to_csv(transformed_current_season, 'Fact_Player.csv')
+    save_to_csv(transformed_previous_season, 'Player_history.csv')
 
 if __name__ == "__main__":
     main()
