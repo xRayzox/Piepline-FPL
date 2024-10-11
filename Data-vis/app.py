4d66bc42b575bc3ec8f752867213c1e2062a105f
Wael Hcine <wael.hcine@esprit.tn>
2024-10-11 16:17:39 +0100

    ssssssssssssssssss

--- Data-vis/app.py
+++ Data-vis/app.py
@@ -1,22 +1,11 @@
-
-# app.py
 import streamlit as st
-import fixture
-import Hello
+from multipage import MultiPage
+from pages import fixture, Hello  # Import your page modules
 
+app = MultiPage()
 
-st.title("My Multi-Page Streamlit App")
-st.sidebar.title("Navigation")
+# Add all pages here
+app.add_page("Fixtures", fixture.app)
+app.add_page("hello", Hello.app)
 
-# Create a sidebar for navigation
-pages = {
-    "hello": Hello,
-    "fixture": fixture,
-}
-
-# Select a page from the sidebar
-selection = st.sidebar.selectbox("Select a page:", list(pages.keys()))
-
-# Display the selected page
-page = pages[selection]
-page.app()  # Call the app function from the respective page module
+app.run()
