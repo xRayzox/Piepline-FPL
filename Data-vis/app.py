290030a7ce73a3258cb96c173e805461fc8e477d
Wael Hcine <wael.hcine@esprit.tn>
2024-10-11 01:58:08 +0100

    pulga

--- Data-vis/app.py
+++ Data-vis/app.py
@@ -9,6 +9,6 @@ if page == "Home":
     st.title("My Multi-Page Streamlit App")
     st.write("Welcome to the multi-page app!")
 elif page == "Page 1":
-    import pages.fixture  # Import your page1.py
+    import fixture  # Import your page1.py
 elif page == "Page 2":
-    import pages.Hello  # Import your page2.py
+    import Hello  # Import your page2.py
