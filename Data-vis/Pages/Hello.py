a65051a562a6aec1fc782652c87fbfd6b9a80674
Wael Hcine <wael.hcine@esprit.tn>
2024-10-11 16:37:18 +0100

    test

--- Data-vis/Pages/Hello.py
+++ Data-vis/Pages/Hello.py
@@ -1,17 +1,17 @@
 import streamlit as st
 
 
-
-st.set_page_config(
+def app():
+    st.set_page_config(
         page_title="Hello",
         page_icon="👋",
     )
 
-st.write("# Welcome to Streamlit! 👋")
+    st.write("# Welcome to Streamlit! 👋")
 
-st.sidebar.success("Select a demo above.")
+    st.sidebar.success("Select a demo above.")
 
-st.markdown(
+    st.markdown(
         """
         Streamlit is an open-source app framework built specifically for
         Machine Learning and Data Science projects.
