de72cfac573151436154707f097be889ba5b687f
Wael Hcine <wael.hcine@esprit.tn>
2024-10-16 15:41:51 +0100

    suiiiiiiiiiiiiiiiiiiiiii

--- FPL/Transformation.py
+++ FPL/Transformation.py
@@ -18,24 +18,49 @@ def transform_fact_history(input_df):
     input_df['value'] = input_df['value'] / 10
     input_df = input_df.rename(columns={
         'round': 'GW',
-        'value': 'price',
+        'value': 'Cost',
         'minutes': 'minutes_played'
     })
 
     return input_df  # Return the transformed DataFrame
 
+
 def transform_gameweeks_data(df):
     # Select needed columns
     needed_columns = ['id', 'name', 'deadline_time', 'highest_score',
                       'average_entry_score', 'most_selected', 'most_transferred_in',
-                      'top_element', 'most_captained', 'most_vice_captained']
-    df = df[needed_columns].copy()  # Create a copy to avoid SettingWithCopyWarning
+                      'top_element', 'most_captained', 'most_vice_captained',
+                      'transfers_made', 'chip_plays']  # Include chip_plays for processing
+    
+    # Create a copy to avoid SettingWithCopyWarning
+    df = df[needed_columns].copy()  
+
+    # Initialize chip columns to zero
+    df['bboost'] = 0
+    df['3xc'] = 0
+    df['wildcard'] = 0
+    df['freehit'] = 0
+
+    # Check for chip plays and update counts for each chip type
+    for index, row in df.iterrows():
+        if isinstance(row['chip_plays'], list) and row['chip_plays']:  # Ensure it's a list and not empty
+            for chip in row['chip_plays']:
+                chip_name = chip['chip_name']
+                if chip_name == 'bboost':
+                    df.at[index, 'bboost'] = chip['num_played']
+                elif chip_name == '3xc':
+                    df.at[index, '3xc'] = chip['num_played']
+                elif chip_name == 'wildcard':
+                    df.at[index, 'wildcard'] = chip['num_played']
+                elif chip_name == 'freehit':
+                    df.at[index, 'freehit'] = chip['num_played']
 
     # Transform 'deadline_time' column
     df['deadline_time'] = pd.to_datetime(df['deadline_time'])
     df = df.rename(columns={'name': 'gw_name'})
+    df.drop(columns=['chip_plays'], inplace=True)
+    return df
 
-    return df 
 
 def transform_previous_season(df):
     # Convert costs from integer to float (e.g., 55 to 5.5)
@@ -46,28 +71,25 @@ def transform_previous_season(df):
 
 def transform_players(players_df):
     # Select only the necessary columns
-    needed_columns = [
-        'code', 'id', 'first_name', 'second_name', 
-        'web_name', 'element_type', 'team', 'team_code', 
-        'dreamteam_count', 'news', 'value_season'
-    ]
-    
-    df = players_df[needed_columns].copy()  # Create a copy to avoid SettingWithCopyWarning
 
+    players_df['now_cost'] = players_df['now_cost'] / 10
+    players_df['photo'] = "https://resources.premierleague.com/premierleague/photos/players/250x250/p" + players_df['photo'].str.replace('.jpg', '.png')    
+    players_df['news_added'] = pd.to_datetime(players_df['news_added'])
     # Replace empty news entries using .loc
-    df.loc[df['news'] == '', 'news'] = 'No news'
-
+    players_df.loc[players_df['news'] == '', 'news'] = 'No news'
+    
     # Create full_name column
-    df['full_name'] = df['first_name'] + ' ' + df['second_name']
+    players_df['full_name'] = players_df['first_name'] + ' ' + players_df['second_name']
 
     # Drop first_name and second_name columns
-    df = df.drop(columns=['first_name', 'second_name'])
+    players_df = players_df.drop(columns=['first_name', 'second_name'])
 
-    return df  # Return the transformed DataFrame for further processing if needed
+    return players_df  # Return the transformed DataFrame for further processing if needed
 
 def transform_positions(positions_df):
     # Select only the necessary columns
-    needed_columns = ['id', 'plural_name', 'plural_name_short', 'singular_name', 'singular_name_short']
+    needed_columns = ['id', 'plural_name', 'plural_name_short', 'singular_name', 'singular_name_short','squad_select',
+                      'squad_min_play', 'squad_max_play']
     df = positions_df[needed_columns].copy()  # Create a copy to avoid SettingWithCopyWarning
 
     return df  
